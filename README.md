e2b3059d1c1522d1e039b4f71471b416798f62cf
Artem Butusov <artem.butusov@unvsoft.com>
2016-04-09 21:18:54 -0400

    Initial version

---
+++ README.md
@@ -0,0 +1,119 @@
+# aurelia-stats #
+
+Plugin aurelia-stats provides digest cycle statistics for Aurelia applications.
+
+Plugin allow to measure duration and size of loops:
+
+- Dirty Checker
+- Micro Task
+- Regular Task
+
+Expected to be used only in browser environment and only for debug purposes.
+
+## Get Started ##
+
+1. Install aurelia-stats:
+
+  ```bash
+  jspm install aurelia-stats
+  ```
+  
+2. Use the plugin in your app's main.js:
+
+  ```javascript
+  export function configure(aurelia) {
+    aurelia.use
+      .standardConfiguration()
+      .developmentLogging();
+   
+    aurelia.use.plugin('aurelia-stats');
+    // or
+    //aurelia.use.plugin('aurelia-stats', options);
+
+    aurelia.start().then(() => aurelia.setRoot());
+  }
+  ```
+  
+3. Include plugin's CSS in your app's index.html:
+  
+  ```css
+  <link rel="stylesheet" href="jspm_packages/github/aurelia-stats@1.0.0/aurelia-stats.css">
+  ```
+
+## Syntax ##
+
+- **DC**: Dirty Checker Loop, *X / Y ms*
+  - X: max number of dirty checked properties during measurement interval
+  - Y: duration of dirty checker loop during measurement interval
+- **MT**: Micro Task Loop, *X / Y ms*
+  - X: total number of micro tasks flushed during measurement interval
+  - Y: duration of micro task loop during measurement interval
+- **RT**: Regular Task Loop, *X / Y ms*
+  - X: total number of regular tasks flushed during measurement interval
+  - Y: duration of regular task loop during measurement interval
+- **SUM**: Summary, *X% / Y ms*
+  - X: how much time framework spend on processing of all loops relative to measurement interval
+  - Y: duration of all loops during measurement interval
+
+## Configuration ##
+
+Plugin options could be passsed like below:
+
+  ```javascript
+  aurelia.use.plugin('aurelia-stats', {
+    threshold: 100,
+    interval: 1000,
+    warnLevel: 0.6,
+    debugDirtyChecker: false
+  });
+  ```
+
+Available options are:
+
+- **threshold**: Max critical duration of digest loop, defaults to 100 ms.
+- **interval**: Update interval, ms, defaults to 1000 ms.
+- **warnLevel**: Warning level relative to threshold, from 0 to 1, defaults to 0.6.
+- **debugDirtyChecker**: Debug dirty checked properties, defaults to false.
+
+## Style ##
+
+Plugin can be styled via CSS.
+
+By default stats sticked to left top corner, but that could be changed, for example, like below:
+
+  ```css
+  .au-stats {
+    left: auto;
+    right: 0;
+  }
+  ```
+
+## Platform Support ##
+
+This library can be used in the **browser** only.
+
+## Building The Code ##
+
+To build the code, follow these steps.
+
+1. Ensure that [NodeJS](http://nodejs.org/) is installed. This provides the platform on which the build tooling runs.
+
+2. From the project folder, execute the following command:
+
+  ```shell
+  npm install
+  ```
+  
+3. Ensure that [Gulp](http://gulpjs.com/) is installed. If you need to install it, use the following command:
+
+  ```shell
+  npm install -g gulp
+  ```
+  
+4. To build the code, you can now run:
+
+  ```shell
+  gulp build
+  ```
+  
+5. You will find the compiled code in the `dist` folder, available in three module formats: AMD, CommonJS and ES6.
---
+++ src/aurelia-stats.js
@@ -0,0 +1,229 @@
+import * as $ from 'jquery';
+import {ObserverLocator} from 'aurelia-binding';
+
+class AureliaStatsPlugin {
+  threshold = 100; 
+  interval = 1000; 
+  warnLevel = 0.6;
+  debugDirtyChecker = false;
+
+  frameworkConfiguration;
+
+  observerLocator;
+
+  rootEl;
+  graphEl;
+  graphCtx;
+  statsEl;
+  dcValueEl;
+  mtValueEl;
+  rtValueEl;
+  sumValueEl;
+
+  width;
+  height;
+  textHeight;
+  canvasHeight;
+  lineWidth;
+
+  currentStats;
+
+  static configure(frameworkConfiguration, pluginConfiguration) {
+    new AureliaStatsPlugin(frameworkConfiguration, pluginConfiguration);
+  }
+
+  constructor(frameworkConfiguration, pluginConfiguration) {
+    this.frameworkConfiguration = frameworkConfiguration;
+
+    if (pluginConfiguration) {
+      if (pluginConfiguration.threshold) {
+        this.threshold = pluginConfiguration.threshold;
+      }
+      if (pluginConfiguration.interval) {
+        this.interval = pluginConfiguration.interval;
+      }
+      if (pluginConfiguration.warnLevel) {
+        this.warnLevel = pluginConfiguration.warnLevel;
+      }
+      if (pluginConfiguration.debugDirtyChecker) {
+        this.debugDirtyChecker = pluginConfiguration.debugDirtyChecker;
+      }
+    }
+
+    this.observerLocator = frameworkConfiguration.container.get(ObserverLocator);
+
+    this.attach();
+  }
+
+  attach() {
+    $(this.attached.bind(this));
+  }
+
+  attached() {
+    this.patchDirtyChecker();
+    this.patchTaskQueue();
+    this.createElement();
+    this.initElement();
+    this.resize();
+    this.scheduleTask();
+  }
+
+  createElement() {
+    this.rootEl = $(`
+      <div class="au-stats">
+        <canvas class="au-stats-graph"></canvas>
+        <div class="au-stats-text">
+          <div class="au-stats-dirty-checker">
+            <div class="au-stats-title">DC: </div>
+            <div class="au-stats-value"></div>
+          </div>
+          <div class="au-stats-micro-tasks">
+            <div class="au-stats-title">MT: </div>
+            <div class="au-stats-value"></div>
+          </div>
+          <div class="au-stats-regular-tasks">
+            <div class="au-stats-title">RT: </div>
+            <div class="au-stats-value"></div>
+          </div>
+          <div class="au-stats-summary">
+            <div class="au-stats-title">SUM: </div>
+            <div class="au-stats-value"></div>
+          </div>
+        </div>
+      </div>
+    `);
+  }
+
+  initElement() {
+    $(document.body).append(this.rootEl);
+
+    this.graphEl = this.rootEl.find('.au-stats-graph');
+    this.graphCtx = this.graphEl[0].getContext('2d');
+
+    this.statsEl = this.rootEl.find('.au-stats-text');
+
+    this.dcValueEl = this.statsEl.find('.au-stats-dirty-checker .au-stats-value');
+    this.mtValueEl = this.statsEl.find('.au-stats-micro-tasks .au-stats-value');
+    this.rtValueEl = this.statsEl.find('.au-stats-regular-tasks .au-stats-value');
+    this.sumValueEl = this.statsEl.find('.au-stats-summary .au-stats-value');
+  }
+
+  resize() {
+    // set context size doesn't work via css, but works via attribute height/width
+    this.graphEl.attr({
+      width: this.graphEl.width(),
+      height: this.graphEl.height()
+    });
+
+    this.width = this.rootEl.width();
+    this.height = this.rootEl.height();
+    this.textHeight = this.statsEl.height();
+    this.canvasHeight = this.height - this.textHeight;
+    this.lineWidth = Math.ceil(this.width / 100);
+    
+    this.clearCurrentStats();
+  }
+
+  patchDirtyChecker() {
+    var self = this;
+
+    var oldCheck = this.observerLocator.dirtyChecker.check;
+
+    this.observerLocator.dirtyChecker.check = function() {
+      self.currentStats.dirtyCheckerSize = Math.max(self.currentStats.dirtyCheckerSize, this.tracked.length);
+      var startTime = Date.now();
+      oldCheck.call(this);
+      self.currentStats.dirtyCheckerLength += Date.now() - startTime;
+    }
+
+    if (this.debugDirtyChecker) {
+      var oldAddProperty = this.observerLocator.dirtyChecker.addProperty;
+
+      this.observerLocator.dirtyChecker.addProperty = function(property) {
+        console.log(`Dirty check property "${property.propertyName}" for object`, property.obj);
+        return oldAddProperty.call(this, property);
+      };
+    }
+  }
+
+  patchTaskQueue() {
+    var self = this;
+
+    var oldFlushMicroTaskQueue = this.observerLocator.taskQueue.flushMicroTaskQueue;
+
+    this.observerLocator.taskQueue.flushMicroTaskQueue = function() {
+      self.currentStats.microTaskQueueSize = Math.max(self.currentStats.microTaskQueueSize, this.microTaskQueue.length);
+      var startTime = Date.now();
+      oldFlushMicroTaskQueue.call(this);
+      self.currentStats.microTaskQueueLength += Date.now() - startTime;
+    };
+
+    var oldFlushTaskQueue = this.observerLocator.taskQueue.flushTaskQueue;
+
+    this.observerLocator.taskQueue.flushTaskQueue = function() {
+      self.currentStats.taskQueueSize = Math.max(self.currentStats.taskQueueSize, this.taskQueue.length);
+      var startTime = Date.now();
+      oldFlushTaskQueue.call(this);
+      self.currentStats.taskQueueLength += Date.now() - startTime;
+    };
+  }
+
+  scheduleTask() {
+    setTimeout(this.task.bind(this), this.interval);
+  }
+
+  clearCurrentStats() {
+    this.currentStats = {
+      dirtyCheckerSize: 0,
+      dirtyCheckerLength: 0,
+      microTaskQueueSize: 0,
+      microTaskQueueLength: 0,
+      taskQueueSize: 0,
+      taskQueueLength: 0
+    };
+  }
+
+  getCurrentStats() {
+    return this.currentStats;
+  }
+
+  task() {
+    var stats = this.getCurrentStats();
+
+    var totalLength = stats.dirtyCheckerLength
+                    + stats.microTaskQueueLength
+                    + stats.taskQueueLength;
+    var totalLoad = Math.round(totalLength / this.interval * 100);
+
+    this.clearCurrentStats();
+
+    var ratio = totalLength / (this.threshold * (this.interval / 1000));
+    if (ratio > 1) {
+      ratio = 1;
+    }
+
+    var color = ratio == 1 ? 'red' : (ratio > this.warnLevel ? 'orange' : 'green');
+
+    var imageData = this.graphCtx.getImageData(this.lineWidth, 0, this.width - this.lineWidth, this.canvasHeight);
+    this.graphCtx.putImageData(imageData, 0, 0);
+
+    this.graphCtx.fillStyle = 'black';
+    this.graphCtx.fillRect(this.width - this.lineWidth, 0, this.lineWidth, this.canvasHeight);
+
+    var size = Math.round(this.canvasHeight * ratio);
+
+    this.graphCtx.fillStyle = color;
+    this.graphCtx.fillRect(this.width - this.lineWidth, this.canvasHeight - size - 1, this.lineWidth, size);
+
+    this.dcValueEl.html(`${stats.dirtyCheckerSize} / ${stats.dirtyCheckerLength} ms`);
+    this.mtValueEl.html(`${stats.microTaskQueueSize} / ${stats.microTaskQueueLength} ms`);
+    this.rtValueEl.html(`${stats.taskQueueSize} / ${stats.taskQueueLength} ms`);
+    this.sumValueEl.html(`${totalLoad}% / ${totalLength} ms`);
+
+    this.scheduleTask();
+  }
+};
+
+export function configure(frameworkConfiguration, pluginConfiguration) {
+  AureliaStatsPlugin.configure(frameworkConfiguration, pluginConfiguration);
+}
